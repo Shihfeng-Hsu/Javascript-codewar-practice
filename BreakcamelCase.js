4184f856ddea257fffa2c887a11d1dd3d8074783
Billy Hsu <ga017281@gmail.com>
2023-02-13 16:08:27 +0800

    first commit

---
+++ BreakcamelCase.js
@@ -0,0 +1,25 @@
+function solution(string) {
+  arr = string.split("");
+  for (i in arr) {
+    if (i != 0) {
+      if (arr[i] == arr[i].toUpperCase()) {
+        arr[i] = [" ", arr[i]];
+      }
+    }
+  }
+  console.log(arr.flat().join(""));
+  return arr.flat().join("");
+}
+
+/*
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+*/
+
+solution("camelCasing"); //,"camel Casing",
+
+solution("camelCasingTest"); //,"camel Casing Test",
---
+++ README.md
@@ -0,0 +1,1 @@
+# Javascript-codewar-practice
